ccb4e5f053d06ea8d44466be708d0583d2081b45
Jason <jason.rgd@gmail.com>
2024-09-27 07:36:22 +1000

    [TEMP] added methods

--- library/install.go
+++ library/install.go
@@ -6,13 +6,36 @@ import (
 )
 
 func Install() bool {
+	setupInstallFolder()
+	cloneRepositories()
+	return true
+}
+
+func setupInstallFolder() bool {
 	installRoot := getInstallationDir()
 	if _, err := os.Stat(installRoot); os.IsNotExist(err) {
 		err := os.Mkdir(installRoot, 0755)
 		if err != nil {
 			fmt.Println("Already Installed or no Permissions ")
+			return false
 		}
 	}
+	return true
+}
+
+func cloneRepositories() bool {
+	return false
+}
+
+func setupPython() bool {
+	return false
+}
+
+func installAnsible() bool {
+	return false
+}
+
+func runAnsiblePlaybook() bool {
 	return false
 }
 
